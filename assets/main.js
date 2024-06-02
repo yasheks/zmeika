d6de3763642995f7433e92a347d9308c1bde93d2
yasheks <yakovzeld@gmail.com>
2024-06-02 21:41:51 +0300

    not working(((

--- assets/main.js
+++ assets/main.js
@@ -2,118 +2,110 @@ const socket = io();
 
 let canvasW;
 let canvasH;
-let max = 0
 
+let loaded_images = 0;
+
+let food = [];
+let tela = [];
+let heads = [];
+let head = [];
+
+const images = {
+    "head": "images/head.png",
+    "telo": "images/telo.png",
+    "eda": "images/eda.png",
+};
 
 let interval;
 
-
 const canvas = document.getElementById("screen");
 
-food = []
+function loadAllImages() {
+    Object.keys(images).forEach((image_title) => {
+        let img = new Image();
 
-socket.on('eda', server_food => {
-    food = server_food
-    console.log(food)
-})
+        img.addEventListener("load", () => {
+            loaded_images += 1;
+            if (loaded_images === Object.keys(images).length) {
+                startGame();
+            }
+        });
 
-window.addEventListener('keydown', event => {
-    console.log(event.key)
-    if (event.key == 'a') {
-        socket.emit('spawn')
-    }
-});
+        img.src = images[image_title];
+        images[image_title] = img;
+    });
+}
 
+function makeFullscreen() {
+    canvas.width = document.body.clientWidth;
+    canvas.height = document.body.clientHeight;
 
+    canvasW = canvas.width;
+    canvasH = canvas.height;
+}
 
-//let VsaHavka = [];
-//let tela = [];
-//
-//
-//let score = 0;
-//let loaded_images = 0;
-//let cadr = 0;
-//
-//const images = {
-//    "head": "images/head.png",
-//    "telo": "images/telo.png",
-//    "eda": "images/eda.png",
-//}
-//
-//
-//
-//
-//function loadAllImages() {
-//    Object.keys(images).forEach((image_title) => {
-//        let img = new Image()
-//
-//        img.addEventListener("load", () => {
-//            loaded_images += 1
-//            if (loaded_images === Object.keys(images).length) {
-//                startGame()
-//            }
-//        });
-//
-//        img.src = images[image_title]
-//        images[image_title] = img
-//    })
-//}
-//
-//
-//
-//
-//function makeFullscreen() {
-//    canvas.width = document.body.clientWidth;
-//    canvas.height = document.body.clientHeight;
-//
-//    canvasW = canvas.width;
-//    canvasH = canvas.height;
-//}
-//function drawBackground(ctx) {
-//    ctx.fillStyle = "#f5f5dc";
-//    ctx.fillRect(0, 0, canvasW, canvasH);
-//}
-//function drawHavka(ctx) {
-//    VsaHavka.forEach((eda) => {
-//        ctx.drawImage(images.eda, eda.x - 30, eda.y - 30, 30, 30);
-//    });
-//}
-//
-//function drawFrame() {
-//    makeFullscreen();
-//    removeOldEda();
-//
-//    const ctx = canvas.getContext("2d");
-//
-//    drawBackground(ctx);
-//
-//
-//    drawHavka(ctx);
-//}
-//
-//
-//function startGame() {
-//    enable();
-//    clearInterval(eda_interval);
-//    setInterval(drawFrame, 20);
-//    eda_interval = setInterval(createEda, 1000);
-//    socket.on('send_eda', (data) => {
-//    console.log(data);
-//
-//    })
-//
-//
-////    addEventListener("mousemove", saveMousePosition);
-////    addEventListener("mousedown", mouseClick);
-//}
-//
-//loadAllImages();
+function drawBackground(ctx) {
+    ctx.fillStyle = "#f5f5dc";
+    ctx.fillRect(0, 0, canvasW, canvasH);
+}
+
+function drawHavka(ctx) {
+    food.forEach((eda) => {
+        ctx.drawImage(images.eda, eda.x - 30, eda.y - 30, 30, 30);
+    });
+}
+
+function drawHead(ctx) {
+    heads.forEach((head) => {
+        ctx.drawImage(images.head, head.x - 30, head.y - 50, 30, 50);
+    });
+}
+
+function move() {
+    window.addEventListener('keydown', event => {
+        console.log(event.key)
+        if (event.key == 'a' || event.key == 'A' || event.key == 'ф' || event.key == 'Ф') {
+            socket.emit('move_left', head);
+        }
+        if (event.key == 'w' || event.key == 'W' || event.key == 'ц' || event.key == 'ц') {
+            socket.emit('move_up', head);
+        }
+        if (event.key == 'd' || event.key == 'D' || event.key == 'в' || event.key == 'В') {
+            socket.emit('move_right', head);
+        }
+        if (event.key == 'ы' || event.key == 'Ы' || event.key == 's' || event.key == 'S') {
+            socket.emit('move_down', head);
+        }
+    });
+}
+
+function drawFrame() {
+
+    socket.on('eda', server_food => {
+        food = server_food;
+
+    });
+    socket.on("head", server_head => {
+        heads = server_head;
+    });
+    makeFullscreen();
+    const ctx = canvas.getContext("2d");
+    drawBackground(ctx);
+    console.log(head)
 
 
+    drawHavka(ctx);
+    drawHead(ctx);
+}
 
-//socket.on("chat message", (data) =>{
-//    const item = document.createElement("li");
-//    item.innerHTML = `<span>${data.name}</span>: ${data.message}`;
-//    messages.appendChild(item);
-//    window.scrollTo(0, document.body.scrollHeight)
-//});
+function startGame() {
+    socket.on("headd", server_head => {
+        head = server_head;
+    });
+
+
+    setInterval(drawFrame, 20);
+}
+
+loadAllImages();
+
--- index.js
+++ index.js
@@ -2,70 +2,66 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-//let VsaHavka = [];
-//let tela = [];
-//let eda_interval;
+
+let sockets = [];
+let food = [];
+let heads = [];
+let playerHead = [];
+
+function random(a, b) {
+    return Math.random() * (b - a) + a;
+}
+
+function addFood() {
+    const eda = {
+        x: random(0, 800),
+        y: random(0, 600),
+    };
+    food.push(eda);
+    sockets.forEach(socket => socket.emit("eda", food));
+}
+
+function removeOldEda() {
+    if (food.length > 15) {
+        food.shift();
+        sockets.forEach(socket => socket.emit("eda", food));
+    }
+}
+function move(){
+    socket.on("move_left", client_head => {
+        playerHead = client_head
+        playerHead[1] -= 5
+    })
+}
+function addHead(socket) {
+    const head = {
+        id: socket.id,
+        x: random(0, 800),
+        y: random(0, 600),
+    };
+    heads.push(head);
+    sockets.forEach(socket => socket.emit("headd", head));
+}
+
+io.on("connection", (socket) => {
+    sockets.push(socket);
+    socket.emit("eda", food);
+    addHead(socket);
+    sockets.forEach(socket => socket.emit("head", heads));
+});
+
+setInterval(() => {
+    addFood();
+    removeOldEda();
+    sockets.forEach(socket => socket.emit("head", heads));
+}, 1000);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
+
 app.use(express.static(__dirname + "/assets"));
 
-
-let sockets = []
-let food = [];
-
-io.on("connection", (socket) => {
-    sockets.push(socket)
-    socket.emit("eda", food);
-    socket.on("spawn", addFood)
-});
-
-function addFood() {
-    food.push(1)
-    sockets.forEach(socket => socket.emit("eda", food))
-}
-
 http.listen(3000, () => {
-    console.log("сервер запущен");
+    console.log("Сервер запущен на порту 3000");
 });
-
-
-//function random(a, b) {
-//    return Math.random() * (b - a) + a;
-//}
-//function createEda() {
-//    const bottle = {
-//        x: random(0, canvasW),
-//        y: random(0, canvasH),
-//    }
-//    bottles.push(bottle)
-//}
-//function removeOldEda() {
-//    if (VsaHavka.length > 15) {
-//        VsaHavka.shift();
-//    }
-//}
-//
-//let sockets = []
-//
-//io.on("connection", (socket) => {
-//    sockets.push(socket)
-//    socket.emit("send_eda", VsaHavka);
-//});
-//
-//function drawFrame() {
-//    removeOldEda();
-//    socket.emit("send_eda");
-//
-//}
-//function startGame() {
-//
-//    clearInterval(eda_interval);
-//    setInterval(drawFrame();, 20);
-//    eda_interval = setInterval(createEda, 1000);
-//    socket.emit("send_eda");
-//    console.log(data);
-//
-//}
-//startGame();
